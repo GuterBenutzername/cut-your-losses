4393cd824ed75d3cc54688697f588c87b5440087
Adam Y. Cole II <afewburritos@gmail.com>
2023-03-10 16:30:27 -0600

    feat: add an assignment card and a list of assignments

---
+++ src/pages/templates/organisms/assignments/assignments.tsx
@@ -0,0 +1,32 @@
+// This component is very simple right now, which means no unit test is neccessary.
+import { type Assignment } from "../../../../backend";
+import AssignmentCard from "../molecules/assignmentCard/assignment";
+import "./assignments.css";
+
+export default function Assignments({
+  assignments,
+  onModifyAssignment,
+  onDeleteAssignment,
+}: {
+  assignments: Assignment[];
+  onModifyAssignment: (
+    event: { target: { value: string } },
+    index: number,
+    property: "name" | "grade" | "weight" | "theoretical"
+  ) => void;
+  onDeleteAssignment: (index: number) => void;
+}) {
+  return (
+    <div className="assignments">
+      {assignments.map((assignment, index) => (
+        <AssignmentCard
+          key={assignment.id}
+          assignment={assignment}
+          index={index}
+          onDeleteAssignment={onDeleteAssignment}
+          onModifyAssignment={onModifyAssignment}
+        />
+      ))}
+    </div>
+  );
+}
