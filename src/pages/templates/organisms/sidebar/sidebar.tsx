26d0967bbff9a76948ab93ee27d5bc4bf412eecf
Adam Y. Cole II <afewburritos@gmail.com>
2023-03-11 15:14:00 -0600

    fix: don't render sidebar when viewport is small

--- src/pages/templates/organisms/sidebar/sidebar.tsx
+++ src/pages/templates/organisms/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 import { type Course } from "../../../../backend";
 import "./sidebar.css";
-import { useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Sidebar({
   courses,
@@ -15,66 +15,92 @@ export default function Sidebar({
 }) {
   const [creating, setCreating] = useState(false);
   const [courseNameText, setCourseNameText] = useState("");
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        setWidth(window.innerWidth);
+      },
+      true
+    );
+    setWidth(window.innerWidth);
+    return () => {
+      window.removeEventListener(
+        "resize",
+        () => {
+          setWidth(window.innerWidth);
+        },
+        true
+      );
+    };
+  }, [width]);
   return (
-    <div className="sidebar">
-      {courses.map((course, index) => (
-        <button
-          key={course.id}
-          className="course-button"
-          type="button"
-          onClick={(event) => {
-            event.currentTarget.blur();
-            onSwapCourse(index);
-          }}
-        >
-          {currentCourse === index && (
-            <span className="selected-course">&gt;</span>
-          )}
-          {course.name}
-        </button>
-      ))}
-      {creating ? (
-        <span className="new-course-button">
-          <label htmlFor="course-input">Course Name</label>
-          <input
-            autoFocus
-            id="course-input"
-            value={courseNameText}
-            className="course-input"
-            onChange={(event) => {
-              setCourseNameText(event.target.value);
-            }}
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                if (courseNameText.trim() === "") {
-                  return;
-                }
+    // React ESLint thinks fragments are unneccessary, despite the fact that they are if the only child is conditionally rendered.
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {width > 600 && (
+        <div className="sidebar">
+          {courses.map((course, index) => (
+            <button
+              key={course.id}
+              className="course-button"
+              type="button"
+              onClick={(event) => {
+                event.currentTarget.blur();
+                onSwapCourse(index);
+              }}
+            >
+              {currentCourse === index && (
+                <span className="selected-course">&gt;</span>
+              )}
+              {course.name}
+            </button>
+          ))}
+          {creating ? (
+            <span className="new-course-button">
+              <label htmlFor="course-input">Course Name</label>
+              <input
+                autoFocus
+                id="course-input"
+                value={courseNameText}
+                className="course-input"
+                onChange={(event) => {
+                  setCourseNameText(event.target.value);
+                }}
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    if (courseNameText.trim() === "") {
+                      return;
+                    }
 
-                onCreateCourse(courseNameText);
-                setCourseNameText("");
-                setCreating(false);
-              }
-            }}
-          />
-        </span>
-      ) : (
-        <button
-          className="new-course-button"
-          type="button"
-          onClick={() => {
-            setCreating(true);
-          }}
-        >
-          New Course
-        </button>
+                    onCreateCourse(courseNameText);
+                    setCourseNameText("");
+                    setCreating(false);
+                  }
+                }}
+              />
+            </span>
+          ) : (
+            <button
+              className="new-course-button"
+              type="button"
+              onClick={() => {
+                setCreating(true);
+              }}
+            >
+              New Course
+            </button>
+          )}
+          <div className="spacer" />
+          <button type="button" className="course-button">
+            Options
+          </button>
+          <button type="button" className="course-button">
+            Help
+          </button>
+        </div>
       )}
-      <div className="spacer" />
-      <button type="button" className="course-button">
-        Options
-      </button>
-      <button type="button" className="course-button">
-        Help
-      </button>
-    </div>
+    </>
   );
 }
