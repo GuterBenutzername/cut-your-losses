74368934b2a56f8ef3937ef20afaf521843c5baa
Adam Y. Cole II <afewburritos@gmail.com>
2023-03-13 08:49:40 -0500

    fix: xo is an idiot
    This is why I never use it directly.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client.js";
+import ReactDOM from "react-dom/client";
 import App from "./pages/app/App";
 import "./index.css";
 import { enablePatches } from "immer";
