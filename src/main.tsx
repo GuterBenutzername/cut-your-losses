d4cec8b18412c942bb03593663df8bfe41c7e78d
Adam Y. Cole II <afewburritos@gmail.com>
2023-03-27 21:02:38 -0500

    feat: add react router in prep for upcoming redesign

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { enablePatches } from "immer";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import App from "./pages/App";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 enablePatches();
 if (typeof document !== "undefined") {
   createRoot(document.querySelector("#root")!).render(
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   );
 }
