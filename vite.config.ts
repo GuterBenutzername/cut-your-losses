bbf2e3b8f360d5a0aafdbccab5d85f58c7107572
Adam Y. Cole II <afewburritos@gmail.com>
2023-03-28 16:42:36 -0500

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+/* eslint-disable spaced-comment */
+/* eslint-disable import/no-anonymous-default-export */
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+
+/// <reference types="vitest" />
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {}
 });
