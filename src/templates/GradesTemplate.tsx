dd0ad07723487bd92dff8c99ae3d5435ecfeaa01
Adam Y. Cole II <afewburritos@gmail.com>
2023-03-30 16:17:47 -0500

    build: switch to styled-components

--- src/templates/GradesTemplate.tsx
+++ src/templates/GradesTemplate.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const GradesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
 function GradesTemplate() {
-  return <div>Grades</div>;
+  return <GradesWrapper>Grades</GradesWrapper>;
 }
 
 export default GradesTemplate;
